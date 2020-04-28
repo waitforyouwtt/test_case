a33b3e718bf092443bcc4e4c00a369a009cc1ebe
wb00001086 <abcd1234>
2020-04-28 19:14:41 +0800

    1. :new: JsonUtils 类库比对 2. :bug: 修复相关bug 3. :wrench: 修改配置文件 4. :fire: 删除不必要的代码

--- src/main/java/com/example/demo/csv/SimpleBeanInfo2.java
+++ src/main/java/com/example/demo/csv/SimpleBeanInfo2.java
@@ -1,17 +1,27 @@
 package com.example.demo.csv;
 
+import com.opencsv.bean.CsvBindByName;
+import com.opencsv.bean.CsvBindByPosition;
+
 /**
  * @Author: luoxian
  * @Date: 2020/4/28 18:36
  * @Email: 15290810931@163.com
  */
-public class SimpleBeanInfo {
+public class SimpleBeanInfo2 {
+
 
     private String header1;
 
+
+
     private String header2;
 
+
     private String header3;
+
+    private String header4;
+
 
     public String getHeader1() {
         return header1;
@@ -37,12 +47,21 @@ public class SimpleBeanInfo {
         this.header3 = header3;
     }
 
+    public String getHeader4() {
+        return header4;
+    }
+
+    public void setHeader4(String header4) {
+        this.header4 = header4;
+    }
+
     @Override
     public String toString() {
-        return "SimpleBeanInfo{" +
+        return "SimpleBeanInfo2{" +
                 "header1='" + header1 + '\'' +
                 ", header2='" + header2 + '\'' +
                 ", header3='" + header3 + '\'' +
+                ", header4='" + header4 + '\'' +
                 '}';
     }
 }
---
+++ src/test/java/com/example/demo/CSVTest.java
@@ -0,0 +1,89 @@
+package com.example.demo;
+
+
+import com.example.demo.csv.SimpleBeanInfo;
+import com.example.demo.csv.SimpleBeanInfo2;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.HeaderColumnNameMappingStrategy;
+import com.opencsv.bean.HeaderColumnNameTranslateMappingStrategy;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.stereotype.Component;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+/**
+ * @Author: 凤凰[小哥哥]
+ * @Date: 2019/7/16 13:51
+ * @Email: 15290810931@163.com
+ */
+@Component
+@Slf4j
+public class CSVTest extends DemoApplicationTests{
+
+    @Test
+    public void lineRead() throws IOException {
+        File destFile = new File("E:/global/fenghuang.csv");
+        CSVReader reader = new CSVReader(new InputStreamReader(new FileInputStream("E:/global/test.csv"),"gbk"));
+        //逐行读取
+        String[] strArr = null;
+        while((strArr = reader.readNext())!=null){
+            System.out.println(strArr[0]+"---"+strArr[1]+"----"+strArr[2]);
+        }
+        reader.close();
+    }
+
+    @Test
+    public void indexNotAnnotionRead() throws FileNotFoundException, UnsupportedEncodingException {
+        CSVReader reader = new CSVReader(new InputStreamReader(new FileInputStream("E:/global/test.csv"), "gbk"));
+        /*
+         *基于列位置，映射成类
+         */
+        //csv文件中的第一列对应类的header，第二列对应类的header2，第三列对应类的header3
+        String[] columnMapping = {"header1", "header2", "header3"};
+        ColumnPositionMappingStrategy<SimpleBeanInfo> mapper = new ColumnPositionMappingStrategy<SimpleBeanInfo>();
+        mapper.setColumnMapping(columnMapping);
+        mapper.setType(SimpleBeanInfo.class);
+        /* */
+        CsvToBean<SimpleBeanInfo> csvToBean = new CsvToBean<SimpleBeanInfo>();
+
+        List<SimpleBeanInfo> list = csvToBean.parse(mapper, reader);
+
+        for (SimpleBeanInfo e : list) {
+            System.out.println(e.toString());
+        }
+    }
+
+    @Test
+    public void indexAnnotionRead() throws FileNotFoundException, UnsupportedEncodingException {
+        CSVReader reader = new CSVReader(new InputStreamReader(new FileInputStream("E:/global/test.csv"),"gbk"));
+        /*
+         * 基于列名转换，映射成类
+         */
+        HeaderColumnNameTranslateMappingStrategy<SimpleBeanInfo2> mapper =
+                new HeaderColumnNameTranslateMappingStrategy<SimpleBeanInfo2>();
+        mapper.setType(SimpleBeanInfo2.class);
+
+        Map<String,String> columnMapping = new HashMap<String,String>();
+        columnMapping.put("header1", "header1");//csv中的header1对应bean的header1
+        columnMapping.put("header2", "header2");
+        columnMapping.put("header3", "header3");
+        columnMapping.put("header4", "header4");
+        mapper.setColumnMapping(columnMapping);
+
+        CsvToBean<SimpleBeanInfo2>  csvToBean = new CsvToBean<SimpleBeanInfo2>();
+
+        List<SimpleBeanInfo2> list = csvToBean.parse(mapper, reader);
+
+        for(SimpleBeanInfo2 e : list){
+            System.out.println(e);
+        }
+    }
+
+}
