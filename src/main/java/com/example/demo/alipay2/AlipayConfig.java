b9b14a0973dbcab774ca98d51acc9e41011f9997
wb00001086 <abcd1234>
2020-05-15 18:50:40 +0800

    1. :new: JsonUtils 类库比对 2. :bug: 修复相关bug 3. :wrench: 修改配置文件 4. :fire: 删除不必要的代码

---
+++ src/main/java/com/example/demo/alipay2/AlipayConfig.java
@@ -0,0 +1,25 @@
+package com.example.demo.alipay2;
+
+/**
+ * @Author: luoxian
+ * @Date: 2020/5/11 16:58
+ * @Email: 15290810931@163.com
+ */
+public class AlipayConfig {
+
+    public static String partner = "2088621920851347";
+
+    public static String private_key = "MIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQCMpIy3b2EYKGwzcwuz/HV9tKzWW2kwWVDpRYTxzpTWdaJ9Rw1ehvnFKAEf/5mTey6GF1uHTU3Cyh9P95J0BagLE+Wc+niR6ua0eWH7hqAPaQ4A6E3zhVEJQsmmDC8ddcQUWg25aUxQiPZSrKlA5wdBLSoErX9cwlkpmJSmGtrjf919c3fDwhWjJ3JlYw0tS+fZ+4OMa4eSBChJtGeqdfowma/eNkJBYPX9GYQL7eH+XENLP9Ua77F6ZwBaO8T4Cx+W5P1hGeNd/Hihn15mOOI8MrVE719zVGuTg0Gq04M2sBuJYf18aqEiWg45Yu6TdAIFf1HM3tXfjffcqDSCNZJ1AgMBAAECggEBAIvw5ztWjev1vMPsw0ZR+64a6qdXyXCcTo/t5nycKyjBX+77lefqsfhk98mZEHzY4s/1ydL1LlyVtVZiZs0fG7f6EkyCRRob+3a0GLSnnff3dkGc5INcsedfFzorgW300ifV+zXz+kVgIojc2ISF3IgTom2JHON1Vja3ZaFps0cNZ9ki/vAuyUx7m9MrVqEWv2supp/f0+P99MHFGc8+PrTqjhB31sYYKPJvIe/FQ1jlOhQwIFznc+wvQ6Uu5Ib7KPtfY7Cd1IW9p5ZYHEewHJkjB3+nEERsnq8jaWazoyCqbCrOxT2CBqkgK2RwXJIS5IVV8kGXnHd1Pn0pLnGa/gECgYEAwzPPF6xxfsSsNrSwVofM2bOLFwWYWMYXSsO93woHVPvpIer0pgv7fW7tDLyzWf+AT4m/lI7xA/df7ANfA/UoTlTEm6mibPBpYNdqCMoeMdciNFMfs1k5RZ5HbBeudLUsALAUaVj9kMn9nGb8C2fhq6Of7O03vK4QJMvsI32cFcECgYEAuHKBkbFM9MLHC+0YdmtHivNvZ2vMUEi/+ZwAvgwduDO4DoX2h1roTh+LgffmH2N9hokAGMxYz3agGV7SWg3nKNMaoVR339A0hAYJ05S45ZOKDxoofUj/+j/Qtp+d5pBZcbCo4lzCz153Y520BmP8t0EBEirK6r9GquEnzp1AcbUCgYEAjZedFnz9bojWqm/yoTGsRBXaRH7IfUxjRNYregfaPajk+LdncrKqX9gAlwahyhxtRESU9Puik8/ygk4Hs+U2sWsHmio4KXFR3u8iePGzaU+1/eJ10jBcPMWM5TlgUEPCgVU32Rq2AzqhfbuuA8/8f/YWYVPI5FIWX7qTtUK934ECgYEAn/Hz9+I3KLzLHMVyhCYNeSoWfimVmD0t9IdagXNCRlzbDAURPlkPv8dNE9xjuu5siMC+y8JQdT+aH8YBRURvIxDFNAdqmDQVhjMyTD7Yg4SIoQX+/H3UNC8oj2Uk6S5d0d/u47Sq3mY9w62a0jvchxEgm59uLVjHMCFlgp8Mpk0CgYBUJ3TRpGOf+79XD+zSM4RcoLAg7c4TBbYq51FWrnsWIAib0VXyNK6Nu7attdF5jbxljeUGAvx1ItvKf1T7b/LG9p33SEjbTXNvu2cgSQJEigYrBLwon+lzRxpHpenE0CjIZIbP3/NvyFn0ae2Gqp9NMeYNAhSR46DyRIf9BMsxPw==";
+
+    public static String ali_public_key  = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAh8l3IngmTE4cs8x14M74eFB3Ot9MiyyIJZeeUGJ2USJPGuFU/7yQzJAVqib39Gqp8ffDNMk0JkYWs/2gUXeFVgQZq37SWTCZLPEprbMofdKqzr790pkYATI/K1Vzzo/x5WQ7dI7+TcHqe86K4x/eKP90J74ICbnx/mhFcZwsdNemRpy1bmsbcDnCb5T71pHrgcE8+rwS2jYWr2JxR+b1SoBCTbgMSlkIMleyCvcw2y0VhJMKVwj5nGiXtLw7+ZSbO9mNLg6pnTkasAZYBnh5jK26bdmrU1kiExZKP7RL5PUU33Ml7oDa9y92rAWAggiBbQc4tFYIKoVLCKTiRb/v9QIDAQAB";
+
+    public static String return_url = "https://8step.name/en/payment/success/?return=66bad2e3-0ed4-4546-a25b-40a8f4f66954";//test
+
+    public static String refer_url = "http://testmerchant.com";
+
+    public static String sign_type = "RSA";
+
+    public static String input_charset = "utf-8";
+
+    public static String service = "create_forex_trade";
+}
--- src/test/java/com/example/demo/ListTest.java
+++ src/test/java/com/example/demo/ListTest.java
@@ -1,11 +1,20 @@
 package com.example.demo;
 
+import com.alibaba.fastjson.JSON;
 import com.example.demo.putong.Roles;
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
 import lombok.extern.slf4j.Slf4j;
+import net.sf.json.JSONObject;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
 import org.springframework.stereotype.Component;
-import java.util.List;
-import java.util.Map;
+
+import java.io.UnsupportedEncodingException;
+import java.math.BigDecimal;
+import java.net.URLEncoder;
+import java.util.*;
 import java.util.stream.Collectors;
 import static com.example.demo.Data.rolesList;
 
@@ -50,5 +59,121 @@ public class ListTest extends DemoApplicationTests{
                 System.out.println("不存在重复值");
             }
         });
+    }
+
+    @Test
+    public void fd(){
+        OrderItem one = new OrderItem();
+        one.setGoodsCode("1001");
+        one.setGoodsName("娃哈哈");
+        one.setQty(new BigDecimal(4));
+        one.setFPaySubAmt(new BigDecimal(4));
+        one.setPPaySubAmt(new BigDecimal(4));
+        one.setPPaidSubAmt(new BigDecimal(4.5));
+
+        OrderItem two = new OrderItem();
+        two.setGoodsCode("1002");
+        two.setGoodsName("洽洽瓜子");
+        two.setQty(new BigDecimal(5));
+        two.setFPaySubAmt(new BigDecimal(5));
+        two.setPPaySubAmt(new BigDecimal(5));
+        two.setPPaidSubAmt(new BigDecimal(5.5));
+
+        List<OrderItem> list = new ArrayList<>();
+        list.add(one);
+        list.add(two);
+        log.info("得到的结果：{}",buildBuyGoodsDetail(list));
+
+        String str = "娃哈哈^4|洽洽瓜子^5";
+        int query = query(str);
+
+        String s = totalQuantity(list);
+        log.info("得到商品总数量：{}",s);
+
+    }
+
+    private String orderItems2GoodDetailsWrapper(List<OrderItem> orderItems) {
+        if (CollectionUtils.isEmpty(orderItems)) {
+            return StringUtils.EMPTY;
+        }
+        StringBuffer sb = new StringBuffer();
+        for (OrderItem orderItem : orderItems) {
+            if (StringUtils.isNotBlank(orderItem.getGoodsCode())
+                    && StringUtils.isNotBlank(orderItem.getGoodsName())
+                    && orderItem.getQty() != null
+                    && orderItem.getPPaidSubAmt() != null) {
+                //实际支付金额
+                BigDecimal unitAmt = orderItem.getPPaySubAmt().add(orderItem.getFPaySubAmt());
+                sb.append("|").append(orderItem.getGoodsCode())
+                        .append(",").append(orderItem.getGoodsName())
+                        .append(",").append(orderItem.getQty())
+                        .append(",").append(unitAmt);
+            }
+        }
+        return sb.length() > 0 ? sb.toString().substring(1) : "";
+    }
+
+    private String buildBuyGoodsDetail(List<OrderItem> orderItems){
+        if (org.springframework.util.CollectionUtils.isEmpty(orderItems)) {
+            return StringUtils.EMPTY;
+        }
+        StringBuffer sb = new StringBuffer();
+        for (OrderItem orderItem : orderItems) {
+            if (StringUtils.isNotBlank(orderItem.getGoodsCode())
+                    && StringUtils.isNotBlank(orderItem.getGoodsName())
+                    && orderItem.getQty() != null
+                    && orderItem.getPPaidSubAmt() != null) {
+                //实际支付金额
+                BigDecimal unitAmt = orderItem.getPPaySubAmt().add(orderItem.getFPaySubAmt());
+                sb.append("|").append(orderItem.getGoodsName()).append("^").append(orderItem.getQty());
+            }
+        }
+        return sb.length() > 0 ? sb.toString().substring(1) : "";
+    }
+
+    private String totalQuantity(List<OrderItem> goodsInfoList){
+        BigDecimal sum = goodsInfoList.stream().map(OrderItem::getQty).reduce(BigDecimal.ZERO, BigDecimal::add);
+        return sum.toString();
+        //  return String.valueOf( goodsInfoList.stream().collect( Collectors.summarizingInt( OrderItem:: getQty) ).getSum() );
+    }
+
+
+
+    //娃哈哈^4|洽洽瓜子^5
+    private int query(String str){
+        List<String> stringList =Splitter.on("|").trimResults().splitToList(str);
+        log.info("得到的数据：{}",stringList);
+        return 0;
+    }
+
+    @Test
+    public void jsonTest(){
+        StringBuffer sb = new StringBuffer();
+        sb.append("12").append("\"").append("xiaogege").append("\"");
+        log.info("字符串前后拼接双引号：{}",sb);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("name","luoxian");
+        map.put("age","29");
+        String jsonObject = JSON.toJSONString(map);
+        log.info("map 转 json:{}",jsonObject);
+    }
+
+    @Test
+    public void requestUrl() throws UnsupportedEncodingException {
+        Map<String, String> requestMap = new HashMap<>();
+        requestMap.put("name","luoxian");
+        requestMap.put("age","29");
+
+        StringBuffer urlSb = new StringBuffer("www.baidu.com");
+        urlSb.append("?");
+        for (String string : requestMap.keySet()) {
+            urlSb.append(string).append("=").append(URLEncoder.encode(requestMap.get(string), "utf-8")).append("&");
+        }
+
+        String requestContent = urlSb.substring(0, urlSb.length() - 1);
+        log.info(">>>>>>>>>>>>>>>>>>   请求报文 ： " + requestContent);
+
+
     }
 }
